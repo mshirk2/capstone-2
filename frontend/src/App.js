5582d2fc48f028c2e5507c03d4a6887df93feb49
Michelle Shirk <michelle.l.shirk@gmail.com>
2023-05-09 17:13:17 -0400

    build API class, user functions, and UserContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import useLocalStorage from './hooks/useLocalStorage';
+import UserContext from './auth/UserContext';
+import ToolLibraryApi from './api';
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [token, setToken] = useLocalStorage("tool-library");
+
+  useEffect(function getUserOnMount(){
+    async function getCurrentUser(){
+      if (token){
+        try {
+          let {username} = jwt.decode(token);
+          ToolLibraryApi.token = token;
+          let currentUser = await ToolLibraryApi.getCurrentUser(username);
+          setCurrentUser(currentUser);
+        } catch (err) {
+          console.error(err);
+          setCurrentUser(null);
+        }
+      }
+    }
+    getCurrentUser()
+  }, [token]);
+
+  async function signup(signupData){
+    try {
+      let token = await ToolLibraryApi.signup(signupData);
+      setToken(token);
+      return { success: true }
+    } catch (errors) {
+      console.error(errors);
+      return {success: false, errors}
+    }
+  }
+
+  async function login(loginData){
+    try {
+      let token = await ToolLibraryApi.login(loginData);
+      setToken(token);
+      return {success: true}
+    } catch (errors) {
+      console.error(errors);
+      return {success: false, errors}
+    }
+  }
+
+  function logout(){
+    setCurrentUser(null);
+    setToken(null);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider
+        value={{
+          currentUser,
+          setCurrentUser
+        }}
+      >
+        <div className="App"></div> // nav bar and routes components
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/api.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+class ToolLibraryApi {
+    static token;
+
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("API Call:", endpoint, data, method);
+
+        // pass auth token through the header
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+        const params = (method === "get")
+            ? data
+            : {};
+    
+        try {
+          return (await axios({ url, method, data, params, headers })).data;
+        } catch (err) {
+          console.error("API Error:", err.response);
+          let message = err.response.data.error.message;
+          throw Array.isArray(message) ? message : [message];
+        }
+    }
+
+    // Individual API routes
+
+    // Get list of tools, with option to filter by tool title
+    static async getTools(title) {
+        let res = await this.request(`tools`, { title });
+        return res.tools;
+    }
+
+    // Get details on a tool by tool id
+    static async getTool(id) {
+        let res = await this.request(`tools/${id}`);
+        return res.tool;
+    }
+
+    // Get current user by username
+    static async getCurrentUser(username) {
+        let res = await this.request(`users/${username}`);
+        return res.user;
+    }
+
+    // User signup
+    static async signup(data) {
+        let res = await this.request(`auth/register`, data, "post");
+        return res.token;
+    }
+
+    // User login
+    static async login(data) {
+        let res = await this.request(`auth/token`, data, "post");
+        return res.token;
+    }
+
+}
+
+// for now we'll put token ("testuser" / "password" on class)
+ToolLibraryApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+
+export default ToolLibraryApi;
